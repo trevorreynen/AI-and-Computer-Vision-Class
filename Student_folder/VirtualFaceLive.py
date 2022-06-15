433ed57a6c1e5be1572a80467d7dfb91bcf591d5
Trevor Reynen <trevorreynen@gmail.com>
2022-06-14 23:41:58 -0400

    See description..
    - Attempted to fix VirtualFaceLive.py. Reason why the program isn't working doesn't make sense and I will need to figure it out later.
    - I tested all other programs involving a camera and they all work including the labs/hw done after VirtualFaceLive.py.
    - Only reasonable explanation to my VirtualFaceLive.py not working is dlib but that isn't mentioned in the errors. The code difference between VirtualFace.py and VirtualFaceLive.py is very small yet VirtualFace.py works fine.

--- Student_folder/VirtualFaceLive.py
+++ Student_folder/VirtualFaceLive.py
@@ -12,7 +12,6 @@ import dlib         # dlib is a toolkit for making machine learning and data ana
 import numpy as np  # NumPy is an important library used for numerical computing.
 import sys          # sys is used to manipulate different parts of the Python runtime environment.
 import os           # os provides functions for interacting with the operating system.
-from time import sleep
 
 
 # The pretrained model that predicts the rectangles that correspond to facial features of a face.
@@ -34,10 +33,13 @@ ALIGN_POINTS = (LEFT_BROW_POINTS + RIGHT_EYE_POINTS + LEFT_EYE_POINTS + RIGHT_BR
 
 # Points from the second image to overlay on the first. The convex hull of each element will
 # be overlaid.
-OVERLAY_POINTS = [LEFT_EYE_POINTS + RIGHT_EYE_POINTS + LEFT_BROW_POINTS + RIGHT_BROW_POINTS, NOSE_POINTS + MOUTH_POINTS]
+OVERLAY_POINTS = [
+    LEFT_EYE_POINTS + RIGHT_EYE_POINTS + LEFT_BROW_POINTS + RIGHT_BROW_POINTS,
+    NOSE_POINTS + MOUTH_POINTS
+]
 
 # Amount of blur to use during colour correction, as a fraction of the pupillary distance.
-COLOR_CORRECT_BLUR_FRAC = 0.6
+COLOUR_CORRECT_BLUR_FRAC = 0.6
 cascade_path = './Haarcascades/haarcascade_frontalface_default.xml'
 cascade = cv2.CascadeClassifier(cascade_path)
 detector = dlib.get_frontal_face_detector()
@@ -45,8 +47,7 @@ predictor = dlib.shape_predictor(PREDICTOR_PATH)
 
 
 def get_landmarks(img, dlibOn):
-    # Returns facial landmarks as (x, y) coordinates.
-    if dlibOn == True:
+    if (dlibOn == True):
         rects = detector(img, 1)
 
         if len(rects) > 1:
@@ -77,24 +78,25 @@ def annotate_landmarks(img, landmarks):
 
     for idx, point in enumerate(landmarks):
         pos = (point[0, 0], point[0, 1])
-        cv2.putText(img, str(idx), pos, cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.4, (0, 0, 255))
-        cv2.circle(img, pos, 3, (0, 255, 255))
+        cv2.putText(img, str(idx), pos, fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, fontScale=0.4, color=(0, 0, 255))
+        cv2.circle(img, pos, 3, color=(0, 255, 255))
 
     return img
 
 
 def draw_convex_hull(img, points, color):
     points = cv2.convexHull(points)
-    cv2.fillConvexPoly(img, points, color)
+    cv2.fillConvexPoly(img, points, color=color)
 
 
 def get_face_mask(img, landmarks):
-    img = np.zeros(img.shape[:2], np.float64)
+    img = np.zeros(img.shape[:2], dtype=np.float64)
 
     for group in OVERLAY_POINTS:
-        draw_convex_hull(img, landmarks[group], 1)
+        draw_convex_hull(img, landmarks[group], color=1)
 
     img = np.array([img, img, img]).transpose((1, 2, 0))
+
     img = (cv2.GaussianBlur(img, (FEATHER_AMOUNT, FEATHER_AMOUNT), 0) > 0) * 1.0
     img = cv2.GaussianBlur(img, (FEATHER_AMOUNT, FEATHER_AMOUNT), 0)
 
@@ -113,8 +115,8 @@ def transformation_from_points(points1, points2):
     points1 = points1.astype(np.float64)
     points2 = points2.astype(np.float64)
 
-    c1 = np.mean(points1, 0)
-    c2 = np.mean(points2, 0)
+    c1 = np.mean(points1, axis=0)
+    c2 = np.mean(points2, axis=0)
     points1 -= c1
     points2 -= c2
 
@@ -135,7 +137,7 @@ def transformation_from_points(points1, points2):
 
 def read_img_and_landmarks(fname):
     img = cv2.imread(fname, cv2.IMREAD_COLOR)
-    img = cv2.resize(img, None, 0.35, 0.35, cv2.INTER_LINEAR)
+    img = cv2.resize(img, None, fx=0.35, fy=0.35, interpolation=cv2.INTER_LINEAR)
     img = cv2.resize(img, (img.shape[1] * SCALE_FACTOR, img.shape[0] * SCALE_FACTOR))
     s = get_landmarks(img, dlibOn)
 
@@ -149,8 +151,8 @@ def warp_img(img, M, dshape):
     return output_img
 
 
-def correct_colors(img1, img2, landmarks1):
-    blur_amount = COLOR_CORRECT_BLUR_FRAC * np.linalg.norm(np.mean(landmarks1[LEFT_EYE_POINTS], 0) - np.mean(landmarks1[RIGHT_EYE_POINTS], 0))
+def correct_colours(img1, img2, landmarks1):
+    blur_amount = COLOUR_CORRECT_BLUR_FRAC * np.linalg.norm(np.mean(landmarks1[LEFT_EYE_POINTS], axis=0) - np.mean(landmarks1[RIGHT_EYE_POINTS], axis=0))
     blur_amount = int(blur_amount)
 
     if blur_amount % 2 == 0:
@@ -162,36 +164,28 @@ def correct_colors(img1, img2, landmarks1):
     # Avoid divide-by-zero errors.
     img2_blur += (128 * (img2_blur <= 1.0)).astype(img2_blur.dtype)
 
-    return img2.astype(np.float64) * img1_blur.astype(np.float64) / img2_blur.astype(np.float64)
+    return (img2.astype(np.float64) * img1_blur.astype(np.float64) / img2_blur.astype(np.float64))
 
 
 def virtual_face(img, name):
     s = get_landmarks(img, True)
 
-    if s == 'error':
+    if (s == 'error'):
         print('No or too many faces')
         return img
 
     img1, landmarks1 = img, s
     img2, landmarks2 = read_img_and_landmarks(name)
 
-    # Produce the Transformation Matrix (M) that maps points from one face to the next.
     M = transformation_from_points(landmarks1[ALIGN_POINTS], landmarks2[ALIGN_POINTS])
 
-    # Produce image mask that outline which parts of image 2 will be overlaid on image 1.
     mask = get_face_mask(img2, landmarks2)
-
-    # Produce warped mask (image 1 with image 2's face overlaid over it.)
     warped_mask = warp_img(mask, M, img1.shape)
-
-    # Produce a combined mask which ensures the features from image 1 are covered up and features
-    # for image 2 are visible.
-    combined_mask = np.max([get_face_mask(img1, landmarks1), warped_mask], 0)
+    combined_mask = np.max([get_face_mask(img1, landmarks1), warped_mask], axis=0)
 
     warped_img2 = warp_img(img2, M, img1.shape)
 
-    # Produce color corrected warped image 2 by matching skin tone and lighting between 2 images.
-    warped_corrected_img2 = correct_colors(img1, warped_img2, landmarks1)
+    warped_corrected_img2 = correct_colours(img1, warped_img2, landmarks1)
 
     # Apply mask to produce final image.
     output_img = img1 * (1.0 - combined_mask) + warped_corrected_img2 * combined_mask
@@ -201,13 +195,15 @@ def virtual_face(img, name):
     fileName = cv2.imwrite(uniqueFile('./images/saved/VirtualFaceLive.jpg'), output_img)
     image = cv2.imread(fileName)
 
-    frame = cv2.resize(image, None, 1.5, 1.5, cv2.INTER_LINEAR)
+    frame = cv2.resize(image, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_LINEAR)
 
     return image
     #return frame
 
 
 # ==========<  Code above was given (I formatted), code below was from lab video.  >==========
+
+cap = cv2.VideoCapture(0)
 
 def uniqueFile(file):
     fName, fExt = os.path.splitext(file)
@@ -224,17 +220,15 @@ def uniqueFile(file):
 # dlibOn controls if use dlib's facial landmark detector (better) or use HAAR Cascade Classifiers
 # (faster). Our base image is taken from webcam. Alight filter_imgage onto the webcam image.
 
-filter_image = './images/single-face1.jpg'
-#filter_image = './images/single-face2.jpg'
+filter_image = './images/single-face2.jpg'
 
 dlibOn = False
-cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
 
     # Reduce image size by 75% to reduce processing time and improve framerates.
-    frame = cv2.resize(frame, None, 0.75, 0.75, cv2.INTER_LINEAR)
+    frame = cv2.resize(frame, None, fx=0.75, fy=0.75, interpolation=cv2.INTER_LINEAR)
 
     # Flip image so that it's more mirror like.
     frame = cv2.flip(frame, 1)
